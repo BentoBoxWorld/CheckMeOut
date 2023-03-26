d758d45d57a9b92e40b7b3bdc668a35e78f0f9a9
BONNe <bonne@bonne.id.lv>
2023-03-26 14:08:14 +0300

    Update to new Bukkit Loader

--- src/main/java/world/bentobox/checkmeout/CheckMeOutPladdon.java
+++ src/main/java/world/bentobox/checkmeout/CheckMeOutPladdon.java
@@ -1,6 +1,5 @@
 package world.bentobox.checkmeout;
 
-import org.bukkit.plugin.java.annotation.dependency.Dependency;
 import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
 import org.bukkit.plugin.java.annotation.plugin.Plugin;
 
@@ -12,9 +11,8 @@ import world.bentobox.bentobox.api.addons.Pladdon;
  *
  * @author BONNe
  */
-@Plugin(name="Pladdon", version="1.0")
+@Plugin(name="CheckMeOut", version="1.0")
 @ApiVersion(ApiVersion.Target.v1_16)
-@Dependency(value = "BentoBox")
 public class CheckMeOutPladdon extends Pladdon {
 
     @Override
